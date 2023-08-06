7b5d30bab8e808c7108405e32db47fa9df71c921
ahmed-debbech <debbech.ahmed@gmail.com>
2023-08-06 12:22:54 +0100

    finish work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', coreRouter);
+
+app.use(cors())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/core.js
+++ routes/core.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
+
 router.get('/num/:n', async function(req, res, next) {
     console.log(req.params.n)
 
@@ -18,13 +19,31 @@ router.get('/num/:n', async function(req, res, next) {
         res.json('internal server error : 500')
         return
     }
-    console.log("*****************************************************************************************")
     console.log(transId)
     axios.get('https://ssowt.orange.tn/WT/mobileconnect/async?TransID='+transId+'&wt-msisdn='+req.params.n)
     .catch(async (err)=>{
         let re2 = await axios.get('https://ssowt.orange.tn'+err.response.data.FallBack)
-        console.log(re2)
+        let trx = re2.data.split('TransID')[2].split('"')[4];
+        
+        axios.get('https://ssowt.orange.tn/WT/OTPConnect/OTPRequest/?TransID='+trx+'&wt-msisdn=216'+req.params.n)
+        res.json({trans : trx})
     })
 });
 
+router.get('/pin/:t/:p', async function(req, res, next) {
+    let re1 = await axios.get('https://ssowt.orange.tn/WT/OTPConnect/status?TransID='+req.params.t+'&confirmationCode='+req.params.p)
+    console.log(re1.headers['set-cookie'])
+    const cookie = (re1.headers['set-cookie'])[0]
+
+    Axios.request({
+        url: "https://myorange.orange.tn/eden/v2/get?language=EN&subsection=account&subsection=balance",
+        method: "get",
+        headers:{
+            Cookie: cookie
+        } 
+        
+   }).then((res)=>{
+        res.json({resp : res.data})
+   })
+});
 module.exports = router;
