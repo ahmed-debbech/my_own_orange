5f32d3c85d5479dd9fb1d86a6064c6afb9c495e5
ahmed-debbech <debbech.ahmed@gmail.com>
2023-08-06 14:09:14 +0100

    catch error

--- routes/core.js
+++ routes/core.js
@@ -6,28 +6,33 @@ const axios = require('axios');
 router.get('/num/:n', async function(req, res, next) {
     console.log(req.params.n)
 
-    let re = await axios.get('https://openid.orange.tn/oidc/authorize?scope=openid&response_type=code&client_id=nMelXvx1upwLvD1ewpGZunz7BnYiyy34&redirect_uri=https%3A//myorange.orange.tn/eden/v2/get&state=auth')
-    console.log(re)
-    let arr = re.data.split('\n')
-    let transId = ""
-    for(let i=0; i<=arr.length-1; i++){
-        if(arr[i].includes('TransID')){
-            transId = arr[i].split('"')[7]
+    try{
+        let re = await axios.get('https://openid.orange.tn/oidc/authorize?scope=openid&response_type=code&client_id=nMelXvx1upwLvD1ewpGZunz7BnYiyy34&redirect_uri=https%3A//myorange.orange.tn/eden/v2/get&state=auth')
+        console.log(re)
+        let arr = re.data.split('\n')
+        let transId = ""
+        for(let i=0; i<=arr.length-1; i++){
+            if(arr[i].includes('TransID')){
+                transId = arr[i].split('"')[7]
+            }
         }
+        if(transId == ''){
+            res.json('internal server error : 500')
+            return
+        }
+        console.log(transId)
+        axios.get('https://ssowt.orange.tn/WT/mobileconnect/async?TransID='+transId+'&wt-msisdn='+req.params.n)
+        .catch(async (err)=>{
+            let re2 = await axios.get('https://ssowt.orange.tn'+err.response.data.FallBack)
+            let trx = re2.data.split('TransID')[2].split('"')[4];
+            
+            axios.get('https://ssowt.orange.tn/WT/OTPConnect/OTPRequest/?TransID='+trx+'&wt-msisdn=216'+req.params.n)
+            res.json({trans : trx})
+        })
+    }catch(error){
+        res.json({error : "error"})
     }
-    if(transId == ''){
-        res.json('internal server error : 500')
-        return
-    }
-    console.log(transId)
-    axios.get('https://ssowt.orange.tn/WT/mobileconnect/async?TransID='+transId+'&wt-msisdn='+req.params.n)
-    .catch(async (err)=>{
-        let re2 = await axios.get('https://ssowt.orange.tn'+err.response.data.FallBack)
-        let trx = re2.data.split('TransID')[2].split('"')[4];
-        
-        axios.get('https://ssowt.orange.tn/WT/OTPConnect/OTPRequest/?TransID='+trx+'&wt-msisdn=216'+req.params.n)
-        res.json({trans : trx})
-    })
+    
 });
 
 router.get('/pin/:t/:p', async function(req, res, next) {
